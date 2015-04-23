5df0ff132c9977f88a3729cfa800b6312e2ce3a5
bmaupin <bmaupincode@gmail.com>
2015-04-23 18:28:49 -0400

    Commit progress

--- toc.go
+++ toc.go
@@ -1,4 +1,12 @@
 package epub
 
 type toc struct {
+    navBodyTemplate = `    <nav epub:type="toc">
+      <h1>Table of Contents</h1>
+      <ol>
+        <li><a href="xhtml/section0001.xhtml">Section 1</a></li>
+      </ol>
+    </nav>
+`
+    xmlnsEpub = `xmlns:epub="http://www.idpf.org/2007/ops"`
 }
