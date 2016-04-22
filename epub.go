4cdb59bc342fe2c12dcfef0372f5a022b9f1311e
bmaupin <bmaupin@users.noreply.github.com>
2016-04-22 14:58:42 -0400

    Start documenting public methods

--- epub.go
+++ epub.go
@@ -12,6 +12,7 @@ const (
 	urnUuid = "urn:uuid:"
 )
 
+// Epub implements an EPUB file
 type Epub struct {
 	author string
 	images map[string]string
@@ -24,6 +25,8 @@ type Epub struct {
 	uuid     string
 }
 
+// NewWriter returns an Epub. A title must be provided as it is a required EPUB
+// element. It can be changed using SetTitle.
 func NewEpub(title string) (*Epub, error) {
 	var err error
 
@@ -68,40 +71,49 @@ func (e *Epub) AddSection(title string, content string) error {
 	return nil
 }
 
+// Author returns the author of the EPUB.
 func (e *Epub) Author() string {
 	return e.author
 }
 
+// Lang returns the language of the EPUB.
 func (e *Epub) Lang() string {
 	return e.lang
 }
 
+// SetAuthor sets the author of the EPUB.
 func (e *Epub) SetAuthor(author string) {
 	e.author = author
 	e.pkg.setAuthor(author)
 }
 
+// SetLang sets the language of the EPUB.
 func (e *Epub) SetLang(lang string) {
 	e.lang = lang
 	e.pkg.setLang(lang)
 }
 
+// SetTitle sets the title of the EPUB.
 func (e *Epub) SetTitle(title string) {
 	e.title = title
 	e.pkg.setTitle(title)
 	e.toc.setTitle(title)
 }
 
+// SetUUID sets the UUID of the EPUB. A UUID will be automatically be generated
+// for you when the NewEpub method is run.
 func (e *Epub) SetUUID(uuid string) {
 	e.uuid = uuid
 	e.pkg.setUUID(uuid)
 	e.toc.setUUID(uuid)
 }
 
+// Title returns the title of the EPUB.
 func (e *Epub) Title() string {
 	return e.title
 }
 
-func (e *Epub) Uuid() string {
+// UUID returns the UUID of the EPUB
+func (e *Epub) UUID() string {
 	return e.uuid
 }
