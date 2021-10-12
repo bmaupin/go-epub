3bcc9b7893d7371ff96058ac24d09d6adcb0c34a
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2021-10-12 17:54:02 +0200

    feat: add full test

--- internal/storage/memory/fs_test.go
+++ internal/storage/memory/fs_test.go
@@ -1,6 +1,8 @@
 package memory
 
 import (
+	"io/ioutil"
+	"path/filepath"
 	"testing"
 )
 
@@ -38,5 +40,36 @@ func TestMemory_Create(t *testing.T) {
 	stat, _ := file.Stat()
 	if !stat.Mode().IsRegular() {
 		t.Fail()
+	}
+}
+
+func TestMemory(t *testing.T) {
+	fs := NewMemory()
+	err := fs.Mkdir("directory", 0666)
+	if err != nil {
+		t.Fatalf("mkdir error %v", err)
+	}
+	err = fs.WriteFile(filepath.Join("directory", "test"), []byte(`content`), 0666)
+	if err != nil {
+		t.Fatalf("writefile error: %v", err)
+	}
+	f, err := fs.Open(filepath.Join("directory", "test"))
+	if err != nil {
+		t.Fatalf("open error: %v", err)
+	}
+	content, err := ioutil.ReadAll(f)
+	if err != nil {
+		t.Fatalf("readall error: %v", err)
+	}
+	if string(content) != "content" {
+		t.Fatal("unexpected content")
+	}
+	err = fs.RemoveAll("directory")
+	if err != nil {
+		t.Fatalf("removeall err: %v", err)
+	}
+	_, err = fs.Open(filepath.Join("directory", "test"))
+	if err == nil {
+		t.Fatal("file should be gone")
 	}
 }
